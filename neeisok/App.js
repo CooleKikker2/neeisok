a439bf5dcdaa9b3f7fc63bfc2f1367dc2de56298
CooleKikker <pctcoen@gmail.com>
2021-09-08 21:51:00 +0200

    Added Files

---
+++ neeisok/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import Navigation from './assets/components/header/navigation';
+
+export default function App() {
+  return (
+    <View>
+      <Navigation>  </Navigation>
+    </View>
+  );
+}
+
---
+++ neeisok/assets/components/header/navigation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Navigation extends Component {
+    render(){
+        return(
+            <Text>Dit is de navigatie!</Text>
+        )
+    }
+}
+
+export default Navigation
